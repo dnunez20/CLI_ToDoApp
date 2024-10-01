c35cce349e902c1d744d09b9dce1a2d45308030b
Daniel Nunez <dannunez97@gmail.com>
2024-10-01 00:44:51 -0500

    Added check for valid input. Cleaned up code

--- TaskCLI.py
+++ TaskCLI.py
@@ -2,51 +2,62 @@ import os
 import sys
 import json
 
-#print("Script name:", sys.argv[0])  # This is the script name
-#print("Arguments:", sys.argv[1:])  
 
-#First check to see if the file exists/and not null
-#Read json object and convert to 
+#check that input is valid. If none are true then program will exit and avoid read/write
+add = True if sys.argv[1] == 'add' and len(sys.argv) == 3 else False
+update = True if sys.argv[1] == 'update' and len(sys.argv) == 4 else False
+delete = True if sys.argv[1] == 'delete' and len(sys.argv) == 3 else False
+
+if not (add or update or delete):
+    sys.exit()
 
 file_path = 'tasks.json'
-jObject = ''
-# check for file path
+tasks = None
 
-##TURN THIS INTO A FUNCTION AND CALL IT IT IN EACH COMMAND
-##WHEN NEEDED.
-## Function will need to return a string
+#First check to see if the file exists/and not null
+#Read json object and convert to dictionary
 if os.path.isfile(file_path) and os.path.getsize(file_path) > 0: 
-   print("Welcome back") 
    with open(file_path, 'r') as file:
         content = file.read()
         tasks = json.loads(content)
 
-num_tasks = len(jObject)
+if add: 
+    if tasks:
+    
+    else:
+    file = open('tasks.json', 'w')
+    arr = {1 : sys.argv[2] }
+
+#num_tasks = len(tasks)
+
     
 
-if sys.argv[1] == 'add' and sys.argv[2] and len(sys.argv[]) == 3:
-    num_tasks += 1
-    tasks[num_tasks] = sys.argv[2]
-    print("Task added successfully")
-
+# if add:
+#     num_tasks += 1
+#     tasks[num_tasks] = sys.argv[2]
+#     print("Task added successfully")
+#     #TODO Write to file
+#     else: 
+#         file = 
+#         tasks.json
 # elif sys.argv[1] == 'update':
 #     print('')
 # elif sys.argv[1] == 'delete':
 #     print('')
-# elif sys.argv[1] == 'mark-in-progress':
-#     print('')
-# elif sys.argv[1] == 'list':
-#     print('')    
+# # elif sys.argv[1] == 'mark-in-progress':
+# #     print('')
+# # elif sys.argv[1] == 'list':
+# #     print('')    
 
-def check_for_file(): #check this before all commands
-    pass
+# def check_for_file(): #check this before all commands
+#     pass
 
-def delete_file(): #only trigger if 1 item on file and it gets deleted
-    pass
+# def delete_file(): #only trigger if 1 item on file and it gets deleted
+#     pass
 
-def create_file(): #if no file exists - Add only
-    pass
-#will only run if exists. Will probably be nested 
-#into check for file
-def read_file(): 
-    pass
+# def create_file(): #if no file exists - Add only
+#     pass
+# #will only run if exists. Will probably be nested 
+# #into check for file
+# def read_file(): 
+#     pass
