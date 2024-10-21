d22b8db35237eec9ab9e65ef2aedf254d010124b
Daniel Nunez <dannunez97@gmail.com>
2024-10-21 00:04:17 -0500

    Started Over. Made it simple
    Tried doing too much when I initially committed. Going to take it 1 step at a time. Added functionality to add a task to a new file and save as JSON

--- TaskCLI.py
+++ TaskCLI.py
@@ -4,60 +4,25 @@ import json
 
 
 #check that input is valid. If none are true then program will exit and avoid read/write
-add = True if sys.argv[1] == 'add' and len(sys.argv) == 3 else False
-update = True if sys.argv[1] == 'update' and len(sys.argv) == 4 else False
-delete = True if sys.argv[1] == 'delete' and len(sys.argv) == 3 else False
+print(sys.argv)
 
-if not (add or update or delete):
-    sys.exit()
+if sys.argv[1] == 'add':
+    add = True
 
 file_path = 'tasks.json'
-tasks = None
 
-#First check to see if the file exists/and not null
-#Read json object and convert to dictionary
-if os.path.isfile(file_path) and os.path.getsize(file_path) > 0: 
-   with open(file_path, 'r') as file:
-        content = file.read()
-        tasks = json.loads(content)
+if add:
+    print("WORKING")
+    tasks = {
+        "1" : sys.argv[2]
+    }
+    with open(file_path, 'w') as json_file:
+        json.dump(tasks, json_file, indent=4)
 
-if add: 
-    if tasks:
-    
-    else:
-    file = open('tasks.json', 'w')
-    arr = {1 : sys.argv[2] }
 
-#num_tasks = len(tasks)
+# #Read json object and convert to dictionary
+# if os.path.isfile(file_path) and os.path.getsize(file_path) > 0:
+#    with open(file_path, 'r') as file:
+#         content = file.read()
+#         tasks = json.loads(content)
 
-    
-
-# if add:
-#     num_tasks += 1
-#     tasks[num_tasks] = sys.argv[2]
-#     print("Task added successfully")
-#     #TODO Write to file
-#     else: 
-#         file = 
-#         tasks.json
-# elif sys.argv[1] == 'update':
-#     print('')
-# elif sys.argv[1] == 'delete':
-#     print('')
-# # elif sys.argv[1] == 'mark-in-progress':
-# #     print('')
-# # elif sys.argv[1] == 'list':
-# #     print('')    
-
-# def check_for_file(): #check this before all commands
-#     pass
-
-# def delete_file(): #only trigger if 1 item on file and it gets deleted
-#     pass
-
-# def create_file(): #if no file exists - Add only
-#     pass
-# #will only run if exists. Will probably be nested 
-# #into check for file
-# def read_file(): 
-#     pass
